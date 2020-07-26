96f586a941a57c846646c45cd68216796cca3c6a
privateos <privateos@foxmail.com>
2020-07-26 19:44:53 +0800

    基于numpy/cupy的深度学习框架(light)5-x

---
+++ ch3/functions.py
@@ -0,0 +1,179 @@
+"""
+    light--a deep learning framework based on numpy/cupy for the purpose of education
+    Copyright (C) 2020  privateos
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from .backend import backend as np
+
+from .operations import Placeholder
+from .operations import Variable
+from .operations import Constant
+from .operations import Add
+from .operations import Argmax
+
+from .operations import Executor
+from .operations import Gradient
+
+def as_ndarray(x):
+    if not isinstance(x, np.ndarray):
+        x = np.array(x)
+    return x
+
+def is_placeholer(x):
+    return isinstance(x, Placeholder)
+
+def is_constant(x):
+    return isinstance(x, Constant)
+
+def is_variable(x):
+    return isinstance(x, Variable)
+
+from .operations import Operation
+def is_operation(x):
+    return isinstance(x, Operation)
+
+from .operations import ArithmeticalOperation
+def is_arithmetical_operation(x):
+    return isinstance(x, ArithmeticalOperation)
+
+from .operations import LogicalOperation
+def is_logical_operation(x):
+    return isinstance(x, LogicalOperation)
+
+def is_light(x):
+    return is_placeholer(x) or is_constant(x) or is_variable(x) or is_operation(x)
+#################################################################################
+def placeholder():
+    return Placeholder()
+
+def variable(initial_value):
+    return Variable(as_ndarray(initial_value))
+
+def constant(value):
+    return Constant(as_ndarray(value))
+
+def add(x, y):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    if not is_light(y):
+        raise TypeError('y is not light type')
+    return Add(x, y)
+
+def argmax(x, axis=None):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    return Argmax(x, axis)
+
+from .operations import MatMul
+def matmul(x, y):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    if not is_light(y):
+        raise TypeError('y is not light type')
+    return MatMul(x, y)
+
+from .operations import Square
+def square(x):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    return Square(x)
+
+from .operations import Mean
+def mean(x, axis=None, keepdims=False):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    return Mean(x, axis=axis, keepdims=keepdims)
+
+from .operations import Sum
+def sum(x, axis=None, keepdims=False):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    return Sum(x, axis=axis, keepdims=keepdims)
+
+from .operations import Subtract
+def subtract(x, y):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    if not is_light(y):
+        raise TypeError('y is not light type')
+    return Subtract(x, y)
+
+from .operations import Multiply
+def multiply(x, y):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    if not is_light(y):
+        raise TypeError('y is not light type')
+    return Multiply(x, y)
+
+from .operations import Divide
+def divide(x, y):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    if not is_light(y):
+        raise TypeError('y is not light type')
+    return Divide(x, y)
+
+from .operations import Negative
+def negative(x):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    return Negative(x)
+
+from .operations import Sqrt
+def sqrt(x):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    return Sqrt(x)
+
+from .operations import Power
+def power(x, y):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    if not is_light(y):
+        raise TypeError('y is not light type')
+    return Power(x, y)
+
+from .operations import Exp
+def exp(x):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    return Exp(x)
+
+from .operations import Log
+def log(x):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    return Log(x)
+
+from .operations import Tanh
+def tanh(x):
+    if not is_light(x):
+        raise TypeError('x is not light type')
+    return Tanh(x)
+
+def executor(*objectives):
+    for n in objectives:
+        if not is_light(n):
+            raise TypeError('an node in objectives is not light type')
+    return Executor(objectives)
+
+def gradient(objective, *variables):
+    if not is_light(objective):
+        raise TypeError('objective is not light type')
+    for n in variables:
+        if not is_light(n):
+            raise TypeError('an node in variables is not light type')
+    return Gradient(objective, variables)
---
+++ ch3/test/LinearRegression.py
@@ -0,0 +1,96 @@
+import sys
+import os
+sys.path.append(os.path.split(os.path.realpath(__file__))[0]+'/../..')
+import ch2 as light
+import ch2.backend as np
+
+def get_variable(shape, dtype):
+    return np.random.uniform(-0.5, 0.5, shape).astype(dtype)
+def get_bais(shape, dtype):
+    return np.zeros(shape, dtype=dtype)
+def mean_squared_error(y, label):
+    loss = light.functions.subtract(y, label)
+    loss = light.functions.square(loss)
+    loss = light.functions.mean(loss, axis=0)
+    loss = light.functions.sum(loss)
+    return loss
+
+class LinearRegression:
+    def __init__(self, input_dims, output_dims, lr=0.001):
+
+        self.x = light.functions.placeholder()
+        self.w = light.functions.variable(get_variable((input_dims, output_dims), dtype=np.float64))
+        self.b = light.functions.variable(get_bais((output_dims, ), dtype=np.float64))
+        self.y = light.functions.add(light.functions.matmul(self.x, self.w), self.b)
+
+        self.label = light.functions.placeholder()
+        self.objective = mean_squared_error(self.y, self.label)
+        self.compute_forward_train = light.functions.executor(self.objective)
+        self.compute_backward_train = light.functions.gradient(self.objective, *[self.w, self.b])
+
+        self.compute_forward_test = light.functions.executor(self.y)
+
+        def fn(ins, d):
+            ins.output_value -= d
+
+        self.fn = fn
+        self.lr = lr
+
+
+    def forward(self, feed_dict):
+        return self.compute_forward_train.run(feed_dict)
+    
+    def backward(self):
+        return self.compute_backward_train.get()
+    
+    def update(self, grads):
+        self.fn(self.w, self.lr*grads[0])
+        self.fn(self.b, self.lr*grads[1])        
+    
+    def fit(self, x, y, epochs=10, batch_size=64, file=sys.stdout):
+        nums = x.shape[0]
+        for i in range(epochs):
+            begin = 0
+            end = batch_size
+            loss = 0.0
+            while end < nums:
+                x_train = x[begin:end]
+                y_train = y[begin:end]
+                beign = end
+                end = end + batch_size
+                feed_dict = {self.x:x_train, self.label:y_train}
+                cur_loss, = self.forward(feed_dict)
+                grads = self.backward()
+                self.update(grads)
+
+                loss += cur_loss/nums
+            print('epochs:'+str(i+1)+'  loss:'+str(loss), file=file)
+    
+    def predict(self, x):
+        feed_dict = {self.x:x}
+        r, = self.compute_forward_test.run(feed_dict)
+        return r
+
+
+from sklearn.datasets import make_classification
+
+x,y = make_classification(n_samples=20000, n_features=2, n_informative=2,
+                          n_redundant=0, n_repeated=0, n_classes=2,
+                          n_clusters_per_class=1, weights=None,
+                          flip_y=0.01, class_sep=1.0, hypercube=True,
+                          shift=0.0, scale=1.0, shuffle=True, random_state=None)
+
+import matplotlib.pyplot as plt                          
+y = y.reshape((y.shape[0], 1))
+LR = LinearRegression(2, 1, lr=0.0005)
+LR.fit(x, y, 40, batch_size=256)
+pred = LR.predict(x)
+p = np.where(pred > 0.5, 1, 0)
+acc = p == y
+print("accuracy="+str(acc.mean()))
+
+plt.scatter(x[:, 0], x[:, 1], c=y[:, 0], s=1)
+plt.show()
+p = np.where(pred > 0.5, 1, 0)
+plt.scatter(x[:, 0], x[:, 1], c=p[:, 0], s=1)
+plt.show()
